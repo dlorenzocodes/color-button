181f99a4a00f833b0447e8d1bcc70dd022605a8e
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-09-07 16:18:07 -0500

    writing my first react test

--- src/App.test.js
+++ src/App.test.js
@@ -1,52 +1,31 @@
-import { getByText, render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
 });
 
 
-// React Testing Library Notes
+test('turns blue when clicked', () => {
+  render(<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'})
 
-render()
-// creates a virtual DOM from argument JSX
-// Access virtual DOM via screen global
-
-getByText()
-// method that finds elements by text
-// pass a regular expression to find the text or string
-
-
-// Jest Assertions
-expect()
-
-.toBeInTheDocument()
-// matcher comes from Jest-DOM
-.toBe()
-.toHaveLength()
-
-// Jest-DOM
-.toBeVisible()
-.toBeChecked()
+  fireEvent.click(colorButton)
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' })
+  expect(colorButton.textContent).toBe('Change to red')
+});
 
 
-// React Testing Library with
-// rendering components into virtual DOM
-// searching virtual DOM
-// interacting with virtual DOM
 
-// Jest 
-// it's a test runner
-// finds tests, run them, make assertions
 
-// TDD
-// writing test before writing code
 
-// React Testing Library
-// creates virtual DOM and provides
-// utilities to interact with the DOM
+
+
+
+
 
 
 
